9740d300772b2b6ada5fb21e1451d85e4f4320ac
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-04-13 23:07:21 +0100

    created vars on character types made psw length prompt

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var specialChar = "' !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var numbers = "1234567890";
+var lowerCase = "abcdefghijklmnopqrsruvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+var pswLength = window.prompt("Please specify the length of your password", "8 to 128 characters long");
+
+generateBtn.addEventListener("click", pswLength)
+
+if (pswLength > 8 && pswLength < 128) {
+  console.log("Well done!");
+} else {
+  pswLength = alert("Password must be between 8 to 128 characters long");
+}
+
+
+
 
 
 // Write password to the #password input
@@ -9,9 +27,14 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
-var specialChar = 
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
